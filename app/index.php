5d9837510e476e3219f72061398acd1ebfa4e90c
Eve <evelina.aguirre16@gmail.com>
2023-11-12 20:44:48 -0300

    Primer commit, esqueleto del proyecto slim-php-mysql-heroku

---
+++ app/index.php
@@ -0,0 +1,42 @@
+<?php
+// Error Handling
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+
+require __DIR__ . '/../vendor/autoload.php';
+require_once './controllers/UsuarioController.php';
+//require_once './db/AccesoDatos.php';
+// require_once './middlewares/Logger.php';
+
+// Instantiate App
+$app = AppFactory::create();
+// Set base path
+$app->setBasePath('/TPComanda/app');
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add parse body
+$app->addBodyParsingMiddleware();
+
+/// Routes
+$app->group('/usuarios', function (RouteCollectorProxy $group) {
+    $group->get('[/]', \UsuarioController::class . ':TraerTodos');
+    $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
+    $group->post('[/]', \UsuarioController::class . ':CargarUno');
+  });
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $payload = json_encode(array("mensaje" => "Slim Framework 4 PHP"));
+    
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
+
